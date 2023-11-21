b741e37eec71692b4c9c13b7d429af986557f2f4
Kevin Ohiro <kevinohiro@gmail.com>
2023-11-21 22:04:33 +0100

    Add: tests for the notifier and repository files

---
+++ time_tracker/test/time_tracker/data/repository/time_tracker_repository_test.dart
@@ -0,0 +1,80 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:time_tracker/core/exceptions/failure.dart';
+import 'package:time_tracker/network_manager/domain/inetwork_manager.dart';
+import 'package:time_tracker/time_tracker/data/datasources/itime_tracker.dart';
+import 'package:time_tracker/time_tracker/data/models/time_tracker.dart';
+import 'package:time_tracker/time_tracker/data/repository/time_tracker_repository.dart';
+
+class MockTimeTrackerDataSource extends Mock
+    implements ITimeTrackerDataSource {}
+
+class MockNetworkManager extends Mock implements INetworkManager {}
+
+void main() {
+  late MockTimeTrackerDataSource mockTimeTrackerDataSource;
+  late TimeTrackerRepository timeTrackerRepository;
+  late MockNetworkManager mockNetworkManager;
+
+  setUp(() {
+    mockTimeTrackerDataSource = MockTimeTrackerDataSource();
+    mockNetworkManager = MockNetworkManager();
+    timeTrackerRepository = TimeTrackerRepository(
+      dataSource: mockTimeTrackerDataSource,
+      networkManager: mockNetworkManager,
+    );
+  });
+
+  group('time tracker repository', () {
+    test("get time spent when network is connected", () async {
+      when(() => mockNetworkManager.isConnected).thenAnswer((_) async => true);
+
+      List<CategoryData> categories = [
+        const CategoryData(
+          category: "daily",
+          data: [
+            Data(
+              title: "title",
+              timeSpent: TimeSpent(current: 5, previous: 7),
+            ),
+          ],
+        ),
+      ];
+
+      when(() => mockTimeTrackerDataSource.getTimeSpent())
+          .thenAnswer((_) async => TimeSpentList(categories));
+
+      final result = await timeTrackerRepository.getTimeSpent();
+
+      expect(
+        result,
+        equals(
+          Right(
+            TimeSpentList(categories),
+          ),
+        ),
+      );
+    });
+
+
+    test(
+        "getTimeSpent with dataSource throwing Failure should return Left(Failure)",
+        () async {
+      when(() => mockNetworkManager.isConnected).thenAnswer((_) async => true);
+
+      final expectedFailure = Failure();
+      when(() => mockTimeTrackerDataSource.getTimeSpent())
+          .thenThrow(expectedFailure);
+
+      final result = await timeTrackerRepository.getTimeSpent();
+
+      expect(
+        result,
+        equals(
+          Left(expectedFailure),
+        ),
+      );
+    });
+  });
+}
---
+++ time_tracker/test/time_tracker/logic/notifier_test.dart
@@ -0,0 +1,121 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:time_tracker/core/constants/string_const.dart';
+import 'package:time_tracker/core/exceptions/failure.dart';
+import 'package:time_tracker/time_tracker/data/models/time_tracker.dart';
+
+import 'package:time_tracker/time_tracker/domain/use_cases/time_tracker_usecase.dart';
+import 'package:time_tracker/time_tracker/logic/notifier.dart';
+import 'package:time_tracker/time_tracker/logic/state.dart';
+
+class MockTimeTrackerUseCase extends Mock implements TimeTrackerUseCase {}
+
+void main() {
+  late MockTimeTrackerUseCase mockTimeTrackerUseCase;
+  late ProviderContainer container;
+  late TimeTrackerNotifier notifier;
+
+  setUp(() {
+    mockTimeTrackerUseCase = MockTimeTrackerUseCase();
+    container = ProviderContainer();
+    notifier = TimeTrackerNotifier(
+      timeTrackerUseCase: mockTimeTrackerUseCase,
+    );
+  });
+
+  tearDown(() {
+    container.dispose();
+  });
+
+  group('time tracker notifier', () {
+    test('should update state to TimeTrackerState.initial() on init', () {
+      expect(notifier.state, equals(const TimeTrackerState.initial()));
+    });
+
+    test(
+        'should update state to TimeTrackerState.loading() when getTimeTracker is called',
+        () {
+      expect(notifier.state, equals(const TimeTrackerState.loading()));
+    });
+
+    test(
+        'should update state to TimeTrackerState.networkException() when there is a connection exception',
+        () async {
+      const connectionExcepMessage = StringConst.connectionException;
+      when(() => mockTimeTrackerUseCase.call()).thenAnswer(
+        (_) async => Left(
+          Failure(message: connectionExcepMessage),
+        ),
+      );
+
+      await notifier.getTimeTracker();
+
+      expect(notifier.state, equals(const TimeTrackerState.networkException()));
+    });
+
+    test(
+        'should update state to TimeTrackerState.error() when there is a connection exception',
+        () async {
+      const errorMessage = "Error";
+      when(() => mockTimeTrackerUseCase.call()).thenAnswer(
+        (_) async => Left(
+          Failure(message: errorMessage),
+        ),
+      );
+
+      await notifier.getTimeTracker();
+
+      expect(
+          notifier.state, equals(const TimeTrackerState.error(errorMessage)));
+    });
+
+    test(
+        'should update state to TimeTrackerState.empty() when the category list is empty',
+        () async {
+      List<CategoryData> categories = [];
+      when(() => mockTimeTrackerUseCase.call()).thenAnswer(
+        (_) async => Right(
+          TimeSpentList(categories),
+        ),
+      );
+
+      await notifier.getTimeTracker();
+
+      expect(notifier.state, equals(const TimeTrackerState.empty()));
+    });
+
+    test(
+        'should update state to TimeTrackerState.data() when the category list is not empty',
+        () async {
+      List<CategoryData> categories = [
+        const CategoryData(
+          category: "daily",
+          data: [
+            Data(
+              title: "title",
+              timeSpent: TimeSpent(current: 5, previous: 7),
+            ),
+          ],
+        ),
+      ];
+      when(() => mockTimeTrackerUseCase.call()).thenAnswer(
+        (_) async => Right(
+          TimeSpentList(categories),
+        ),
+      );
+
+      await notifier.getTimeTracker();
+
+      expect(
+        notifier.state,
+        equals(
+          TimeTrackerState.data(
+            data: TimeSpentList(categories),
+          ),
+        ),
+      );
+    });
+  });
+}
